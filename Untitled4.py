e387ccb8f56c7f89a923d8dbe2557a50a736aeaa
ziqinliu <146738679+ziqinliu@users.noreply.github.com>
2023-10-03 19:32:49 +0100

    Add files via upload

---
+++ Untitled4.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[1]:
+
+
+print("Introduction to Programming")
+
+
+# In[2]:
+
+
+a="ziqin liu"
+b="ucbvzql@ucl.ac.uk"
+c="22"
+print(a+'\n'+b+'\n'+c)
+
+
+# In[3]:
+
+
+print(0)
+print(178)
+print(-21)
+print(2938/49)
+print(436*9948)
+print(12**20)
+
+
+# In[5]:
+
+
+import math
+print(math.sin(200/math.pi*180))
+print(math.cos(100/math.pi*180))
+print(math.tan(45/math.pi*180))
+
+
+# In[9]:
+
+
+a="Ziqin"
+b="Liu"
+print(a)
+print(b)
+print(a+' '+b)
+
+
+# In[12]:
+
+
+a=float(str(1))
+if a>0:
+    print("a is positive")
+elif a<0:
+    print("a is negative")
+else:
+    print("a is zero")
+    
+
+
+# In[33]:
+
+
+import random
+def guess_num(1,20):
+    random_number=random.randint(1,20)
+    while true:
+    print('Congratulations,it is true!')
+
+
+# In[ ]:
+
+
+
+
